0d39f2f50bc094f64a3ab7454ea1c4f7d4b58b7f
jake <jake@rmgr.dev>
2025-12-18 09:32:47 +1030

    Set up nvim debugging configs

--- debugger.lua
+++ debugger.lua
@@ -1,16 +1,16 @@
--- We set up local lua debugger, this is optional but suggested!
-
+-- Local Lua Debugger for nvim-dap
 if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
-   require("lldebugger").start()
-end
+	-- Load the debugger from global location
+	local home = os.getenv("HOME")
+	local lldebugger = assert(loadfile(home .. "/.local/bin/lldebugger/debugger/lldebugger.lua"))()
+	lldebugger.start()
 
-if os.getenv "LOCAL_LUA_DEBUGGER_VSCODE" == "1" then
-   local lldebugger = require "lldebugger"
-   lldebugger.start()
-   local run = love.run
-   --- @diagnostic disable-next-line
-   function love.run(...)
-       local f = lldebugger.call(run, false, ...)
-       return function(...) return lldebugger.call(f, false, ...) end
-   end
+	-- Wrap love.run to catch errors
+	local run = love.run
+	function love.run(...)
+		local f = lldebugger.call(run, false, ...)
+		return function(...)
+			return lldebugger.call(f, false, ...)
+		end
+	end
 end
